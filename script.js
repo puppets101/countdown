cf24192bc21a4b7cf42a1ee2a33583e9603b5170
puppets101 <oscand92@gmail.com>
2020-11-03 23:11:18 +0100

    add calculations to the page

--- script.js
+++ script.js
@@ -6,10 +6,24 @@ function countDown() {
 
   const totalSeconds = (birthdayDate - currentDate) / 1000;
 
+  // Calculate times
   const days = Math.floor(totalSeconds / 3600 / 24);
-  const hours = Math.floor(totalSeconds / 3600 % 24) + 1;
+  const hours = Math.floor(totalSeconds / 3600 % 24);
   const minutes = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds % 60);
+
+  // Get elements from the DOM
+  const daysEl = document.getElementById('days');
+  daysEl.innerText = days;
+
+  const hoursEl = document.getElementById('hours');
+  hoursEl.innerText = hours;
+
+  const minutesEl = document.getElementById('minutes');
+  minutesEl.innerText = minutes;
+
+  const secondsEl = document.getElementById('seconds');
+  secondsEl.innerText = seconds;
 
 
   console.log(days, hours, minutes, seconds);
